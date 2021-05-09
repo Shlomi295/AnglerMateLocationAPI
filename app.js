9802fce6374bcc50b2155304bf68221fe9e12c59
Shlomi Moreh <shlomi295@gmail.com>
2021-05-09 16:49:59 +1000

    updates

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-require('dotenv/config');
+require('dotenv/config'); //load the config file env, can be used by calling process.env.{variableName} 
+const url = "mongodb+srv://Angler_User:89CL735AU@sit725.63pic.mongodb.net/AM_Fish?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.DB_CONNECTION,  { useUnifiedTopology: true, useNewUrlParser:true } , () => console.log('Connected to DB'));
+var express = require("express");
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url,{ useUnifiedTopology: true, useNewUrlParser: true });
+
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.DB_CONNECTION,  { useUnifiedTopology: true, useNewUrlParser:true } , () => console.log('Connected to DB'));
+
+let fishes;
+client.connect(err => {
+  fishes = client.db("AM_Fish").collection("FishRegs");
+  console.log("Connected to database");
+  //    client.close();
+});
 
 app.get('/', (req, res) => {
     res.send("we are at the homepage")
 });
 
-const PORT = process.env.PORT|3000;
+app.get("/ping", function (request, response) {
+    var user_name = request.query.name;
+    response.json("Pong!");
+  });
+
+  
+app.use(bodyParser.urlencoded({extended:false}));
+const PORT = process.env.PORT || 8088; 
+
 app.listen(PORT);
 console.log(`Listening on port: ${PORT}`)
