37b7fec718295ca7a05875aca653ac4f0b1d464a
Shlomi Moreh <shlomi295@gmail.com>
2021-05-09 15:54:20 +1000

    Added db connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+require('dotenv/config');
+
+mongoose.connect(process.env.DB_CONNECTION,  { useUnifiedTopology: true, useNewUrlParser:true } , () => console.log('Connected to DB'));
 
 app.get('/', (req, res) => {
     res.send("we are at the homepage")
