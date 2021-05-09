dd6c04835f1e73539170dc72e0f0d423a494ead9
Shlomi Moreh <shlomi295@gmail.com>
2021-05-09 15:29:11 +1000

    CICD commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("we are at home")
+    res.send("we are at the homepage")
 });
 
 const PORT = process.env.PORT|3000;
